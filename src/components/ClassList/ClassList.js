06cd2e992aa886ecb6c79500edfa4b14a78536c5
Bradley Service <bradleykservice@gmail.com>
2020-11-05 15:07:20 -0600

    react-router-afternoon commit

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 export default class ClassList extends Component {
   constructor() {
     super()
     
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount(){
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+    .then(res => {
+      this.setState({
+        students: res.data
+      })
+    })
+    .catch(err => console.log(err))
   }
 
   render() {
+    const mappedStudents = this.state.students.map((elem, i) => {
+      return <Link to={`/student/${elem.id}`} key={i}><h3>
+        {elem.first_name} {elem.last_name}
+      </h3></Link>
+    })
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {mappedStudents}
       </div>
     )
   }
